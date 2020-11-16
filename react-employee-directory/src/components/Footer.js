f846321995c6effbb4312f355720fb331dc62563
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-11-16 18:15:21 +1030

    wrote functions for components

---
+++ react-employee-directory/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return(
+        <div style={{ marginLeft: "40px" }}>
+            <h2>Employee Directory by <a href='https://github.com/MK-Kwon/'>Min Kyung Kwon</a></h2>
+        </div>
+    );
+}
+
+export default Footer;
