87639e2ff9e41d157f2583d4a1a725d13e752ea0
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-11-16 23:22:26 +1030

    added some changes to style the page

--- react-employee-directory/src/components/Footer.js
+++ react-employee-directory/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return(
         <div style={{ marginLeft: "40px" }}>
-            <h2>Employee Directory by <a href='https://github.com/MK-Kwon/'>Min Kyung Kwon</a></h2>
+            <h3>Employee Directory by <a href='https://github.com/MK-Kwon/'>Min Kyung Kwon</a></h3>
         </div>
     );
 }
